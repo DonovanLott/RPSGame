7410cc632dadc42e70ba6d049647070da3331d61
fidelA2021 <145623542+fidelA2021@users.noreply.github.com>
2023-11-15 16:15:40 -0500

    Initial Scotty commit
    Trying to see if it commits properly with what i have now.

---
+++ Assets/scisssorMove1.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scisssorMove1 : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private Camera mainCam;
+    private Rigidbody2D rb;
+    public float force;
+
+    public bool canMove = true;
+    public bool isMoving = false;
+    public float timeBetweenMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Debug.Log(isMoving);
+
+        rb = GetComponent<Rigidbody2D>();
+
+        if (Input.GetMouseButtonUp(0)) {
+           
+            canMove = false;
+            isMoving = true;
+
+            mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+            
+            mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 direction = mousePos - transform.position;
+            rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+            
+            
+            
+        }
+
+        if(isMoving) {
+            StartCoroutine(Wait());
+            if (force > 0f) {
+                force = force - .025f; 
+            } else if (force <= 0f) {
+                force = 0f;
+                rb.velocity = new Vector2(0, 0);
+                isMoving = false;
+                canMove = true;
+            }
+
+            
+
+        }
+
+
+        
+
+        if(Input.GetMouseButton(0) && canMove ) {
+
+
+
+            if (force <= 15f) {
+                force = force + .05f; 
+            } else if (force >= 15f) {
+                force = 15f;
+            }
+
+
+            
+
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1f);
+        
+       
+        //playerSprite.transform.localPosition = Vector3.MoveTowards(playerSprite.transform.localPosition, myPos, 100f * Time.deltaTime);
+
+        
+    }
+}
