2aa3429644603dff13dca7cca4d56777b6f2d5ab
Thomas Nguyen <120068504+tNguyen311@users.noreply.github.com>
2023-11-21 18:58:49 -0500

    Revert "Merge branch 'Thomas_PariMovement' of https://github.com/DonovanLott/RPSGame into Thomas_PariMovement"
    This reverts commit a82689857763bba420f07bfaa9bb2ae64582e2b8, reversing
    changes made to 7ff858301f8a3515fa3652fbcddd174521d532f2.

--- Assets/Scripts/PariMovementScripts/PariMovement.cs
+++ Assets/Scripts/PariMovementScripts/PariMovement.cs
@@ -25,7 +25,7 @@ public class PariMovement : MonoBehaviour
     public float RotationSpeed;
 
     int way;
-    int flipz = 0;
+    public int flipz = 0;
     bool spinz = false;
 
     public Vector2 minVal, maxVal;
@@ -38,16 +38,20 @@ public class PariMovement : MonoBehaviour
     Vector2 movementM = new Vector2(-.25f, 0);
     Vector2 movementM2 = new Vector2(.25f, 0);
     Vector2 movementM3 = new Vector2(0, -.25f);
+
+
     
     void Start()
     {
         currHealth = health;
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+
 
         if(currHealth <= 0)
         {
